c8ef31c40b4afd8c95d1b16e28504ab306ba7f70
LHchien33 <judy263562@gmail.com>
2024-12-12 00:38:58 +0800

    loaded promise & add background sources

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="video-background">
-    <video autoplay loop muted>
-      <source src="./assets/bg.mp4" type="video/mp4">
+    <video ref="video" autoplay loop muted playsinline poster="@/assets/bg.jpg">
+      <source src="@/assets/bg.mp4" type="video/mp4" media="(min-width: 1200px)">
+      <source src="@/assets/bg_720.mp4" type="video/mp4" media="(min-width: 768px)">
+      <source src="@/assets/bg_480.mp4" type="video/mp4">
     </video>
-    <MusicPlayer @loaded="handleLoaded"/>
+    <MusicPlayer @loaded="onMusicPlayerLoaded"/>
     <transition name="fade">
       <Loading v-if="isLoading"/>
     </transition>
@@ -11,8 +13,8 @@
 </template>
 
 <script>
-import MusicPlayer from './components/MusicPlayer.vue';
-import Loading from './components/Loading.vue';
+import MusicPlayer from '@/components/MusicPlayer.vue';
+import Loading from '@/components/Loading.vue';
 
 export default {
   components: {
@@ -22,12 +24,36 @@ export default {
   data() {
     return {
       isLoading: true,
+      videoLoaded: null,
+      musicPlayerLoaded: null,
     };
   },
   methods: {
-    handleLoaded() {
-      this.isLoading = false;
+    onMusicPlayerLoaded() {
+      this.musicPlayerLoaded = Promise.resolve();
+    },
+    async waitForAllLoaded() {
+      try {
+        await Promise.all([this.videoLoaded, this.musicPlayerLoaded]);
+        setTimeout(() => {
+          this.isLoading = false;
+        }, 1000);
+      } catch (err) {
+        console.error('Error during loading');
+      }
     },
   },
+  mounted() {
+    this.videoLoaded = new Promise((resolve) => {
+      const video = this.$refs.video;
+      if (video.readyState >= 3) {
+        resolve();
+      } else {
+        video.addEventListener('canplay', resolve, { once: true });
+      }
+    });
+
+    this.waitForAllLoaded();
+  }
 };
 </script>
